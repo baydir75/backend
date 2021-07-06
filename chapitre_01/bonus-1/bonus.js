7952ff592a3890b801a813ba0062d8b580e6dd91
baydir <kidoni@live.fr>
2021-07-06 17:11:22 +0200

    bonus 1

---
+++ chapitre_01/bonus-1/bonus.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const responseAPICountries = require('./dataCountries.js');
+const port = 8000;
+app.listen(port, () => {
+  console.log('Server started on port: ' + port);
+});
+
+app.get('/', (req, res) => {
+  res.send("Authors API");
+});
+
+app.get('/country/:item', (req, res) => {
+   res.json(responseAPICountries.responseAPICountries[req.params.item]);
+});
